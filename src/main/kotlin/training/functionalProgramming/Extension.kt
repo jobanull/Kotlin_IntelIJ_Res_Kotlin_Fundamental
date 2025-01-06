ac8a729f8327f55a2764c399d9dd541d63314d00
cyptrid <jikhdan@gmail.com>
2025-01-07 06:03:01 +0700

    Revise Extension

--- src/main/kotlin/training/functionalProgramming/Extension.kt
+++ src/main/kotlin/training/functionalProgramming/Extension.kt
@@ -2,31 +2,25 @@ package training.functionalProgramming
 
 import org.junit.Test
 
+// TODO 1
+// Buat fungsi printInt untuk ekstensi Integer, lalu cetak valuenya.
 
-fun Int.printInt() {
-    // TODO 1
-    // Implementasikan fungsi printInt() untuk mencetak "value $this".
-}
+// TODO 2
+// Buat fungsi ekstensi Int plusThree yang mengembalikan nilai Int.
+// Implementasikan fungsi tersebut agar menambahkan 3 pada nilai integer.
 
-fun Int.plusThree(): Int {
-    // TODO 2
-    // Implementasikan fungsi plusThree() yang menambahkan 3 pada nilai integer.
-    return 0
-}
-
-val Int.slice: Int
-    get() = this / 2 // TODO 3
-// Implementasikan properti slice untuk membagi nilai integer dengan 2.
+// TODO 3
+// Buat properti ekstensi slice pada Int yang mengembalikan nilai setengah dari integer.
 
 
 class ExtensionTest {
     @Test
     fun setTest() {
         // TODO 4
-        // Panggil printInt() pada nilai 10 dan cetak hasilnya.
+        // Panggil fungsi printInt() pada nilai 10 dan cetak hasilnya.
 
         // TODO 5
-        // Panggil plusThree() pada nilai 20 dan cetak hasilnya.
+        // Panggil fungsi plusThree() pada nilai 20 dan cetak hasilnya.
 
         // TODO 6
         // Panggil properti slice pada nilai 30 dan cetak hasilnya.
