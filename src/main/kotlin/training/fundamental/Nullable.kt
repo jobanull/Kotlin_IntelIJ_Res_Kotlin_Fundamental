ca2a171bf78d502e004757a8116964aee0583dd6
cyptrid <jikhdan@gmail.com>
2025-01-09 04:46:11 +0700

    Save

--- src/main/kotlin/training/fundamental/Nullable.kt
+++ src/main/kotlin/training/fundamental/Nullable.kt
@@ -8,22 +8,28 @@ class Nullable {
         // Periksa apakah parameter `name` tidak `null` dan tampilkan nilai `name`.
 
         // TODO 2
-        // Deklarasikan variabel nullable `loc` dan coba akses panjangnya dengan safe call.
+        // coba akses panjangnya loc dengan safe call.
+        val loc = String
 
         // TODO 3
         // Gunakan elvis operator (`?:`) untuk memberikan nilai default jika `text` null.
+        val text = String
+        val textLength = String
+
+        println(textLength)
 
         // TODO 4
         // Gunakan not-null assertion (`!!`) untuk memaksa akses panjang `testing`.
+        val testing = String
     }
-
-    // Nullable Receiver
-    val Int?.slice2: Int
-        get() =
-        // TODO 5
-            // Tambahkan logika untuk membagi `this` dengan 2 jika tidak null, atau kembalikan 0.
-            0 // Placeholder
 }
+
+// Nullable Receiver
+val Int?.slice2: Int
+    get() =
+    // TODO 5
+        // Tambahkan logika untuk membagi `this` dengan 2 jika tidak null, atau kembalikan 0.
+        0
 
 class NullableTest {
     val nullableTest = Nullable()
@@ -32,5 +38,6 @@ class NullableTest {
     fun nullableTest() {
         // TODO 6
         // Panggil fungsi `nullable` dengan parameter yang sesuai untuk menguji hasil.
+        nullableTest.nullable("Joban")
     }
 }
