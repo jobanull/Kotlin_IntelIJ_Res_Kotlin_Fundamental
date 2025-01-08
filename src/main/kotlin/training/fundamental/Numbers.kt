bce4ca1355402fc754cfd7edae53391785302159
cyptrid <jikhdan@gmail.com>
2025-01-09 04:47:55 +0700

    Save

--- src/main/kotlin/training/fundamental/Numbers.kt
+++ src/main/kotlin/training/fundamental/Numbers.kt
@@ -6,24 +6,37 @@ class Numbers {
     fun numbers() {
         // TODO 1
         // Deklarasikan variabel `long` dengan tipe `Long` dan beri nilai.
+        val long = Long
 
         // TODO 2
         // Deklarasikan variabel `short` dengan tipe `Short` dan beri nilai.
+        val short = Short
 
         // TODO 3
         // Deklarasikan variabel `byte` dengan tipe `Byte` dan beri nilai maksimum.
+        val byte = Byte
 
         // TODO 4
         // Deklarasikan variabel `double` dengan tipe `Double` dan beri nilai.
+        val double = Double
 
         // TODO 5
         // Deklarasikan variabel `float` dengan tipe `Float` dan beri nilai.
+        val float = Float
 
         // TODO 6
         // Deklarasikan variabel `maxLong` untuk menyimpan nilai maksimum dari tipe `Long`.
+        val maxLong = Long
 
         // TODO 7
         // Cetak nilai dari semua variabel yang telah dideklarasikan di atas.
+
+        println(long)
+        println(short)
+        println(byte)
+        println(double)
+        println(float)
+        println(maxLong)
     }
 }
 
@@ -32,7 +45,6 @@ class NumbersTest {
 
     @Test
     fun numbersTest() {
-        // TODO 8
-        // Panggil fungsi `numbers` untuk memastikan semua tipe data numerik bekerja sesuai harapan.
+        numbers.numbers()
     }
 }
