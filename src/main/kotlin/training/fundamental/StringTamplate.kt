87b90eb5ec5ae3e4a017f399b5515fc838c05959
cyptrid <jikhdan@gmail.com>
2025-01-09 04:54:37 +0700

    Save

--- src/main/kotlin/training/fundamental/StringTamplate.kt
+++ src/main/kotlin/training/fundamental/StringTamplate.kt
@@ -3,21 +3,18 @@ package training.fundamental
 import org.junit.Test
 
 class StringTemplate {
-    fun stringTemplate(name: String, age: Int): String {
-        // TODO 1
-        // Gunakan string template untuk mengembalikan kalimat "My name is $name, i'm $age years old".
-        return ""
-    }
+
+    // TODO 1
+    // Buat fungsi stringTemplate yang meminta parameter name dan age dan mengembalikan string
+    // Gunakan string template untuk mengembalikan kalimat "My name is $name, i'm $age years old".
+
 }
 
 class StringTemplateTest {
     val stringTemplate = StringTemplate()
-
     @Test
     fun stringTemplateTest() {
-        // TODO 2
-        // Panggil fungsi `stringTemplate` dengan parameter nama dan usia.
-        // TODO 3
-        // Cetak hasilnya ke konsol.
+        val value = stringTemplate
+        println(value)
     }
 }
