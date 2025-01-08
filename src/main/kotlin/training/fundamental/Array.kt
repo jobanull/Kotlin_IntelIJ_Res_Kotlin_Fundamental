e06f69d351383d0ffd7a1760038915c70274776a
cyptrid <jikhdan@gmail.com>
2025-01-09 03:52:26 +0700

    test

--- src/main/kotlin/training/fundamental/Array.kt
+++ src/main/kotlin/training/fundamental/Array.kt
@@ -6,30 +6,39 @@ class Array {
     fun array() {
         // TODO 1
         // Buat array dengan elemen campuran (integer, string, boolean).
-        val mixArray = 0
+        val mixArray = arrayOf(1,"Joban", false,3)
 
         // TODO 2
         // Buat array khusus dengan elemen bertipe integer.
-        val intArray = 0
+        val intArray = intArrayOf(1,2,3,2,4)
 
         // TODO 4
         // Buat array khusus dengan elemen bertipe double.
-        val doubleArray = 0
+        val doubleArray = doubleArrayOf(0.1, 0.2)
 
         // TODO 5
         // Ubah elemen keempat dari `intArray` menjadi 80.
-
+        intArray[4] = 80
         // TODO 6
         // Cetak elemen-elemen dari intArray menggunakan `forEach` loop.
-
+        intArray.forEach { println(it) }
         // TODO 7
-        // Cetak elemen-elemen dari array campuran menggunakan `for` loop.
-
+        // Cetak elemen-elemen dari mixArray menggunakan `for` loop.
+        for( i in mixArray){
+            println(i)
+        }
         // TODO 8
         // Cetak elemen-elemen dari `intArray` dengan indeks menggunakan `for` loop.
-
+        for((i, e) in intArray.withIndex()){
+            println("Index $i, Element $e")
+        }
         // TODO 9
         // Cetak elemen-elemen dari `doubleArray` menggunakan `while` loop.
+        var index = 0
+        while (index < doubleArray.size){
+            println(doubleArray[index])
+            index++
+        }
     }
 }
 
--- src/main/kotlin/training/fundamental/Boolean.kt
+++ src/main/kotlin/training/fundamental/Boolean.kt
@@ -7,31 +7,48 @@ class Boolean {
     fun bool1(hour: Int) {
         // TODO 1
         // Tentukan apakah jam (hour) berada dalam rentang 7 hingga 23.
+        val result : Boolean = if(hour in 7..23){
+            true
+        }else{
+            false
+        }
 
         // TODO 2
         // Cetak hasil evaluasi boolean tersebut.
+
+        println(result)
     }
 
     fun bool2(hour: Int) {
         // TODO 3
         // Tentukan apakah jam (hour) berada di luar rentang 7 hingga 18.
+        val result = if(hour< 7 || hour > 18){
+            true
+        }else {
+            false
+        }
 
         // TODO 4
         // Cetak hasil evaluasi boolean tersebut.
+        println(result)
     }
 
     fun bool3(hour: Int) {
         // TODO 5
         // Buat variabel `open` dengan nilai 7.
+        val open = 7
 
         // TODO 6
         // Tentukan apakah jam (hour) lebih besar dari `open`.
+        val newResult = hour > open
 
         // TODO 7
         // Gunakan operator `!` untuk membalikkan hasil evaluasi.
+        val result = !newResult
 
         // TODO 8
         // Cetak hasil evaluasi boolean tersebut.
+        println(result)
     }
 }
 
@@ -42,17 +59,20 @@ class BooleanTest {
     fun boolTest1() {
         // TODO 9
         // Panggil fungsi `bool1` dengan parameter tertentu.
+        booleanTest.bool1(10)
     }
 
     @Test
     fun boolTest2() {
         // TODO 10
         // Panggil fungsi `bool2` dengan parameter tertentu.
+        booleanTest.bool2(2)
     }
 
     @Test
     fun boolTest3() {
         // TODO 11
         // Panggil fungsi `bool3` dengan parameter tertentu.
+        booleanTest.bool3(3)
     }
 }
